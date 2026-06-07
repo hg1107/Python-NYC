237990d37100149b5edcb17305f5d5cec24bc7f5
hg1107 <hardeepsghotra8181@gmail.com>
2026-06-07 19:35:58 +0530

    Understood and implemented type conversion using int(), bool(), float(), str()

--- 3_string_and_type_conversion.py
+++ 3_string_and_type_conversion.py
@@ -22,3 +22,32 @@ print(task[6:9])
 print(task[14:])
 # Hello:
 print(task[0:5])
+
+
+
+# Type Conversion:
+a = "12"
+print(a)
+print(type(a))
+
+b = int(a)  # Can also assign to same variable after type conversion
+c = float(a)
+d = str(a)
+e = bool(a)
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+print(d)
+print(type(d))
+
+print(e)
+print(type(e))
+
+for_bool = 0
+print(bool(for_bool))
+
+# Boolean gives false for 0, true for every other
