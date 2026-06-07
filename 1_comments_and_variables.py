928c38cd720bb0287bed2d4fac2a4f016efc6f27
hg1107 <hardeepsghotra8181@gmail.com>
2026-06-07 18:42:04 +0530

    Understood the rules and naming conventions for creating a variable, and successfully completing chapter 1 for the tutorial

--- 1_comments_and_variables.py
+++ 1_comments_and_variables.py
@@ -4,3 +4,18 @@
     A multi line comment
 """
 
+# Using variables to store some data, can be string, integer, boolean, etc
+var_name = "This is a variable"
+
+"""
+    Rules for creating variables:
+    1. Cannot start with a number
+    2. No spaces in between
+    3. Cannot use special characters
+"""
+
+# Naming conventions:
+
+camelCase = "Start new word with caps"
+PascalCase = "Start even the first word with caps"
+snake_case = "Using _ to differ between the words"
