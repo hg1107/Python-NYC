ee4fb53075e0920d99bde7eec16f6ca16c9629c1
hg1107 <hardeepsghotra8181@gmail.com>
2026-06-08 19:58:41 +0530

    Started with chapter 5 and understood and implemented if-else statement, and if-elif ladder

---
+++ 5_if_else.py
@@ -0,0 +1,16 @@
+age = int(input("Enter your age: "))
+
+# If - else
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+
+# If - elif - else ladder
+if age > 18:
+    print("You are adult")
+elif age >=13:
+    print("You are teenager")
+else:
+    print("You are child")
