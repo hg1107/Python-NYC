49309eb27d51875e20e9c47253a5548d6216530d
hg1107 <hardeepsghotra8181@gmail.com>
2026-06-08 20:58:06 +0530

    Finished with understanding for loop, now moving towards break, continue, and else

---
+++ 6_for_loop.py
@@ -0,0 +1,29 @@
+# Range function:
+# Syntax: range(start, stop, step)
+
+for i in range(0, 101, 5):
+    print(i, end=" ")
+
+
+n = int(input("\nEnter a number: "))
+for i in range(n, (n * 10) +1, n):
+    print(i, end = " ")
+
+
+print("\n")
+
+
+a = "Students"
+for i in a:
+    print(i, end = " ")
+
+print("\n")
+
+for i in range(len(a)):
+    print(f"{i}: {a[i]}")
+
+print("\n")
+
+
+
+# Break:
