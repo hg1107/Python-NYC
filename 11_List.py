84e94948c0cac1022bedcd41beaebd1ea38c8565
hg1107 <hardeepsghotra8181@gmail.com>
2026-06-24 13:33:15 +0530

    done with lists, now starting with its methods

---
+++ 11_List.py
@@ -0,0 +1,38 @@
+'''
+
+Collection of multiple data types
+list = []
+Ordered
+Indexed
+Mutable
+Duplications allowed
+
+'''
+
+
+list = [1, 'Hello', True, 2.5]
+# Complete list
+print(list)
+print("\n")
+
+# Indexing
+print(list[1])
+print("\n")
+
+# Mutable
+list[-1] = 3.14
+print(list)
+print("\n")
+
+# Traversing (loops) on lists
+a = [10, 20, 30, 40, 50]
+
+# 1. Traversing on values:
+for i in a:
+    print(i, end = " ")
+print("\n")
+
+# 2. Traversing on indexes
+for i in range(len(a)):
+    print(f"{i}: {a[i]}")
+print("\n")
