f3b55077cfb994240a6a01a7cc51039619874edc
hg1107 <hardeepsghotra8181@gmail.com>
2026-06-24 13:05:49 +0530

    Understood 3 types of arguments, hence concluding the chapter

--- 9_Functions.py
+++ 9_Functions.py
@@ -7,6 +7,9 @@ hello()
 hello()
 
 
+print("\n")
+
+
 a = 123
 def reverse(n):
     rev = 0
@@ -19,8 +22,45 @@ print(reverse(a))
 print(reverse(456))
 
 
+print("\n")
+
+
 def addition(a, b):
     return a + b
 
 print(addition(2, 3))
 print(addition(123, 456))
+
+
+print("\n")
+
+
+# Types of arguments
+
+# 1. Positional
+def multiply(a, b, c, d):
+    print(a * b * c * d)
+
+# multiply(1, 2, 3)   -->  Will give error
+multiply(1, 2, 3, 4)
+
+
+print("\n")
+
+
+# 2. default
+def addition(a, b, c = 10):
+    print(a + b + c)
+
+addition(1, 2)
+addition(1, 2, 3)
+
+
+print("\n")
+
+
+# 3. Keyword
+def subtraction(a, b):
+    print(b - a)
+
+subtraction(b = 30, a = 10)
